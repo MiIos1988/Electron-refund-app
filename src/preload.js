c6e82b3b5669f18bcd58fb88ef6483b243af6ac6
Milos1988 <vojvoda1988@gmail.com>
2023-08-07 23:06:04 +0200

    Finish electron app

--- src/preload.js
+++ src/preload.js
@@ -3,13 +3,25 @@ const { ipcRenderer } = require('electron');
 document.addEventListener('DOMContentLoaded', function () {
 
     const xmlFilesInput = document.getElementById('xmlFilesInput');
+    const msg = document.getElementById('msg');
     const myButton = document.getElementById('myButton');
+    xmlFilesInput.addEventListener("change", (event) => {
+        const selectedFiles = event.target.files;
+        console.log(selectedFiles)
+        if (selectedFiles.length > 0) {
+            msg.textContent = `Dodali ste ${selectedFiles.length} fajl(a).`;
+          } else {
+            msg.textContent = 'Niste dodali nijedan fajl.';
+          }
+        
+    })
 
     myButton.addEventListener('click', () => {
         const files = Array.from(xmlFilesInput.files);
         const filePaths = files.map((file) => file.path);
 
         ipcRenderer.send("saveText", filePaths);
+        msg.textContent = ""
     });
 
 });
